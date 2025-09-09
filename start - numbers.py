d225e506b0280d6994094fa8824ac0421a6be2a5
Arthur <arthurpcanuto@gmail.com>
2025-09-09 18:00:18 -0300

    Simple number exercise

---
+++ start - numbers.py
@@ -0,0 +1,14 @@
+number = 239 + 588
+result = number ** 2
+print(result)
+
+number = (239 + 588) ** 2
+print(number)
+
+print((239 + 588) ** 2)
+
+number = 239 + 588
+result = number * number
+print(result)
+
+print((239 + 588) * (239 + 588))
