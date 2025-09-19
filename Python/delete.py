5e240869fcf66e8c3d1e0ecea62e5b6ad6b38948
Arthur <arthurpcanuto@gmail.com>
2025-09-19 17:49:59 -0300

    moving forward with chapter 6

--- Python/delete.py
+++ Python/delete.py
@@ -9,3 +9,48 @@ print(alien_0['color'])
 alien_0 = {'color': 'green', 'points': 5}
 new_points = alien_0['points']
 print(f"You just earned {new_points} points!")
+
+#Adding New Key-Value Pairs to a Dictionary
+alien_0 = {'color': 'green', 'points': 5}
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+#Building the alien_0 dictionary when it starts empty
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+#Modifying Values in a Dictionary
+alien_0 = {'color': 'green'}
+print(f"The alien color is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien color is {alien_0['color']}.")
+
+#More Practical use of modifying dictionaries
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original Position: {alien_0['x_position']}")
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    #This must be the fast alien
+    x_increment = 3
+
+    #The new position is the old postion + the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New position: {alien_0['x_position']}")
+
+#Removing a key from a dictionary
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+del alien_0['points']
+print(alien_0)
