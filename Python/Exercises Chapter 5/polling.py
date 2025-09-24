580eee0915dcccc124b1e78cbb63386c9bd67670
Arthur <arthurpcanuto@gmail.com>
2025-09-23 22:48:42 -0300

    added an alternative to the 'polling' exercise

--- Python/Exercises Chapter 5/polling.py
+++ Python/Exercises Chapter 5/polling.py
@@ -7,8 +7,18 @@ favorite_languages = {
 
 should_poll = ['jen', 'edward', 'mike', 'anna', 'sarah', 'phil', 'tom', 'kimi']
 
+#Alternative 1
 for name in should_poll:
     if name in favorite_languages:
         print(f"Thanks for taking the poll {name.title()}!")
     else:
         print(f"Please {name.title()}, take the poll.")
+
+print("\n")
+
+#Alternative 2
+for name in should_poll:
+    if name not in favorite_languages:
+        print(f"Please {name.title()}, take the poll.")
+    elif name in favorite_languages:
+        print(f"Thanks for taking the poll {name.title()}!")
