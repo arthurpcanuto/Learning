1c6c239362ac6d607aab4f7255ed95dad5233635
Arthur <arthurpcanuto@gmail.com>
2025-09-30 00:00:02 -0300

    three exits exercise from chapter 7 (turns out I ended up doing it before hand in the movie tickets exercise)

--- Python/Exercises Chapter 7/three_exits.py
+++ Python/Exercises Chapter 7/three_exits.py
@@ -1,15 +1,16 @@
 active = True
 while active:
-    action = input("Would you like some tickets? Y/N")
+    action = input("Would you like to continue? Y/N")
     action = action.lower()
+
     if action == 'y':
         age = int(input("How old are you?"))
         if age <= 3:
-            print("Your ticket is free of charge!")
+            print("Your tickets are free of charge!")
         elif age > 3 and age <= 12:
-            print("Your ticket will be $10.")
+            print("You tickets will be $10!")
         else:
-            print("Your ticket will be $15.")
+            print("Your tickets will be $15!")
     
     if action == 'n':
         break
